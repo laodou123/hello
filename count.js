7f06f6c7e3bdc4a4df9db635eb90fd93c7a9e25f
laodou123 <13858860788a@gmail.com>
2023-10-19 20:17:24 +0800

    hello world

---
+++ count.js
@@ -0,0 +1,9 @@
+<script>
+        for (let i = 1; i <= 10; i++) {
+            if (i % 2 !== 0) {
+                document.writeln(i); 
+            } else {
+                alert("Even number: " + i);
+            }
+        }
+    </script>
